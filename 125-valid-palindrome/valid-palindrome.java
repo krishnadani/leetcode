734699cfb96f27d85b5b052b1dd8dd6490d33e3a
Krishna Dani <krishnagdani@gmail.com>
2026-09-19 23:59:32 +0530

    Time: 2 ms (99.31%) | Memory: 44.4 MB (62.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,17 +5,14 @@ class Solution {
 
         while(left < right) {
 
-            // Skip non-alphanumeric from left
             while(left < right && !Character.isLetterOrDigit(s.charAt(left))) {
                 left++;
             }
 
-            // Skip non-alphanumeric from right
             while(left < right && !Character.isLetterOrDigit(s.charAt(right))) {
                 right--;
             }
 
-            // Compare lowercase characters
             if(Character.toLowerCase(s.charAt(left)) !=
                Character.toLowerCase(s.charAt(right))) {
                 return false;
